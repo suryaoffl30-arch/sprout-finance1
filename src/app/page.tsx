5f6b515f2e35109811c33770d5001488eec3829a
Surya P <suryapremraj30@gmail.com>
2025-10-04 15:12:00 +0000

    PUSH EVERYTHING TO GITHUB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function LoginPage() {
               </Label>
               <Input id="email" type="email" placeholder="name@example.com" />
             </div>
-            <Button asChild className="w-full font-bold">
+            <Button className="w-full font-bold" asChild>
               <Link href="/analysis">Continue with Email</Link>
             </Button>
             <div className="relative">
